de409b7e83ff29b3f0c2455d83515bb7d8738d25
Guillermo San Arranz <guillermo.san@bain.com>
2024-05-11 15:54:20 -0500

    Adding hugging face successful LLM

---
+++ hugging_face.py
@@ -0,0 +1,14 @@
+from langchain_community.llms import HuggingFaceEndpoint
+
+import os
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+
+llm = HuggingFaceEndpoint(
+    repo_id="mistralai/Mistral-7B-Instruct-v0.2")
+
+# The LLM takes a prompt as an input and outputs a completion
+our_query = "What is the currency of India?"
+
+#Submit prompt to the LLM
+completion = llm.invoke(our_query)
+print("\nResponse:", completion)
